46b9362baba68e16e272832642a70d015dabcf68
Liooouu <apiadojohnliol@gmail.com>
2025-07-26 20:58:21 +0800

    added minimal changes

--- my-react-app/src/Dashboard.jsx
+++ my-react-app/src/Dashboard.jsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {
   return (
-    <div className="p-10">
+    <div className="p-10 flex justify-center text-center">
       <h1 className="text-2xl font-bold">Sobrang Pogi ni Lio!</h1>
       <p className="mt-4">You have successfully logged in.</p>
     </div>
