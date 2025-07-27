33eb4d69c59f81a021a7c1168d13372684e3e9d9
Liooouu <apiadojohnliol@gmail.com>
2025-07-27 23:25:21 +0800

    added blue text and normal text

--- my-react-app/src/Dashboard.jsx
+++ my-react-app/src/Dashboard.jsx
@@ -3,6 +3,7 @@ export default function Dashboard() {
     <div className="p-10 flex justify-center text-center">
       <h1 className="text-2xl font-bold">Sobrang Pogi ni Lio!</h1>
       <p className="mt-4">You have successfully logged in.</p>
+      Normal text<span className="text-blue-600 font-semibold underline"> Blue Text</span>
     </div>
   );
 }
