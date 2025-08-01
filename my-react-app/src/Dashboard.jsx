78d43fd81b587166bd03908a6e309a60b700672a
Liooouu <apiadojohnliol@gmail.com>
2025-08-01 13:06:39 +0800

    01/08/25

--- my-react-app/src/Dashboard.jsx
+++ my-react-app/src/Dashboard.jsx
@@ -2,6 +2,7 @@ export default function Dashboard() {
   return (
     <div className="p-10 flex justify-center text-center">
       <h1 className="text-2xl font-bold">Sobrang Pogi ni Lio!</h1>
+      <h2 className="text-2xl font-semibold text-center">Pang dag dag push</h2>
       <p className="mt-4">You have successfully logged in.</p>
       Normal text<span className="text-blue-600 font-semibold underline"> Blue Text</span>
     </div>
